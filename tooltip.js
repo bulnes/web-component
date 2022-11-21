be0e42d30c4668c7dedfbb43976e60571ff33cf0
Bruno Bulnes <bulnes.brno@gmail.com>
2022-11-20 21:48:22 -0300

    func: setting support to custom attribute

--- tooltip.js
+++ tooltip.js
@@ -4,9 +4,14 @@ class Tooltip extends HTMLElement {
     super();
 
     this._tooltipContainer
+    this._tooltipText = 'Some dummy default text here.'
   }
 
   connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text')
+    }
+
     const tooltipIcon = document.createElement('span')
     tooltipIcon.textContent = ' (?) '
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
@@ -17,7 +22,7 @@ class Tooltip extends HTMLElement {
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div')
-    this._tooltipContainer.textContent = 'This is the tooltip text!'
+    this._tooltipContainer.textContent = this._tooltipText
     this.appendChild(this._tooltipContainer)
   }
 
