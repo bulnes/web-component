9bfd223edf680899123f6978e335b5c596ec2f2e
Bruno Bulnes <bulnes.brno@gmail.com>
2022-12-07 00:16:21 -0300

    feat: adding basic style and function to modal

--- modal.js
+++ modal.js
@@ -23,6 +23,8 @@ class Modal extends HTMLElement {
           height: 100vh; /* com isso o body nao precisa ser 100% ;) */
           background-color: rgba(0,0,0,0.75);
           z-index: 10;
+          opacity: 0;
+          pointer-events: none;
         }
 
         #modal {
@@ -30,16 +32,56 @@ class Modal extends HTMLElement {
           top: 15vh;
           left: 25%;
           width: 50%;
-          height: 30rem;
           z-index: 100;
           background-color: white;
           border-radius: 3px;
           box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          opacity: 0;
+          pointer-events: none;
+        }
+
+        header {
+          padding: 1rem;
+        }
+
+        header h1 {
+          font-size: 1.25rem;
+        }
+
+        #main {
+          padding: 1rem 1rem 2rem;
+        }
+
+        #actions {
+          border-top: 1px solid #ccc;
+          padding: 1rem;
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        #actions button {
+          margin: 0 0.25rem;
         }
       </style>
-      
+
       <div id="backdrop"></div>
-      <div id="modal"></div>
+      <div id="modal">
+        <header>
+          <h1>Please Confirm</h1>
+        </header>
+
+        <section id="main">
+          <slot></slot>
+        </section>
+
+        <section id="actions">
+          <button>Cancel</button>
+          <button>Okay</button>
+        </section>
+      </div>
     `;
   }
 }
